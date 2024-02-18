c45277fd544cb514eccdf0aa0db119cd27ebb3db
repmacrypn <artik3267@gmail.com>
2024-02-18 13:01:45 +0300

    fix: update rollup config (input file)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -55,7 +55,7 @@ export default [
     ],
   },
   {
-    input: 'dist/esm/types/index.d.ts',
+    input: 'dist/esm/src/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     external: ['styled-components'],
     plugins: [dts(), dotenv()],
